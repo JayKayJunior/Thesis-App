9e2e4d6f5df896ede50d482a7dd5a92a9a26c795
Jan Kwiatkowski <jaykayjunior10@gmail.com>
2022-10-09 01:15:11 +0200

    save player info in DB

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -1,35 +1,52 @@
 <?php
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use App\Entity\Player;
+use App\Database\DatabaseBroker;
+use App\Repository\PlayerRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 class PlayerController extends AbstractController
 {
+    private $em;
+    private $playerRepository;
 
+    public function __construct(playerRepository $playerRepository, EntityManagerInterface $em){
+        $this->playerRepository = $playerRepository;
+        $this->em = $em;
+
+    }
     #[Route('/player/{tagLine}/{gameName}', name: 'app_player',methods:['GET','HEAD'])]
     public function index($tagLine,$gameName,HttpClientInterface $httpClient): Response
     {
-        $player = new Player();
-        $player->setGameName($gameName);
-        $player->setTagLine($tagLine);
-
-        (new \App\ThirdPartyAPI\APILoLGet)->getLoLAccontByNick($player, $httpClient);
-
-        if ($player->getPuuid() == NULL) {
-            return $this->render('player/player_not_found.html.twig', [
-                'gameName' => $player->getGameName(),
-                'tagLine' => $player->getTagLine(),
-            ]);
+        $array = $this->playerRepository->findByGameName($gameName,$tagLine);
+        if($array == []) {
+            $player = new Player();
+            $player->setGameName($gameName);
+            $player->setTagLine($tagLine);
+            (new \App\ThirdPartyAPI\APILoLGet)->getLoLAccontByNick($player, $httpClient);
+            if ($player->getPuuid() == NULL) {
+                return $this->render('player/player_not_found.html.twig', [
+                    'gameName' => $player->getGameName(),
+                    'tagLine' => $player->getTagLine(),
+                ]);
+            }else{
+                $this->em->persist($player);
+                $this->em->flush();
+            }
+        }else{
+            $player = $array[0];
         }
 
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($player);
-        $entityManager->flush();
+
+
 
 
         return $this->render('player/player.html.twig', [
